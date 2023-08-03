596e2ecb25f1257beedb57cdf49b6c466bbe9afa
pravinpb <pravin.sarana@gmail.com>
2023-08-03 12:24:02 +0530

    basic JS

---
+++ app.js
@@ -0,0 +1,32 @@
+var pravin = 'hello world'
+console.log(pravin)
+var height = window.innerHeight
+var width = window.innerWidth
+console.log(height)
+console.log(width)
+window.alert('close it')
+window.confirm('Yes or No')
+
+a = 'djfd'
+console.log(a)
+
+
+
+i = 0
+while(i<10) {
+    console.log(i)
+    i++
+}
+
+console.log("\n")
+
+for (i = 0; i <10; i++) {
+    console.log(i)
+}
+
+console.log("\n")
+
+l1 = [10,20,30,40]
+l1.forEach(i => {
+    console.log(i)
+})
